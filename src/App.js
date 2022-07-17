c74ef3e20752ea5b9977bb76f8a68ef393e97dbb
suraj.agariya@ext.religare.in <suraj.agariya@ext.religare.in>
2022-07-17 15:05:43 +0530

    "Header with Scss"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import Header  from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
-    <div>
-      Header
-    </div>
+    <Router>
+      <Header></Header>
+      <Routes>
+      <Route path='/' exact element={<Home/>}/>
+      <Route path='/movie/:mid' exact element={<MovieDetail/>}/>
+      <Route element={<PageNotFound/>}></Route>    
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import user from '../../asset/images/user.png'
+import  './Header.scss'
 
 const Header = () => {
     return (
-        <div>
-                Header
-        </div>
+        <header className='header'>
+            <Link to="/">
+            <div className='logo'>
+               Movie App
+            </div>
+            </Link>
+            <div className='user-image'>
+            <img src={user} alt="user" ></img>
+            </div>
+        </header>
     );
 };
 
