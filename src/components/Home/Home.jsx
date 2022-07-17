1eb995268df6e19c31bb54c5cf3c1f663ec11a0e
suraj.agariya@ext.religare.in <suraj.agariya@ext.religare.in>
2022-07-17 21:01:36 +0530

    "Make Api Call for Axios"

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import './Home.scss';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+
+     const makeApiCall= async ()=>{
+        let searchTerm="Harry";
+        let type = "movie";
+        let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchTerm}&type=${type}`
+        console.log(url);
+        let response = await axios.get(url);
+        let searchData = response.data;
+        console.log(searchData);
+    }
+    useEffect(()=>{
+        makeApiCall();
+    },[]);
     return (
-        <div>
+        <div className='home'>
             Home
         </div>
     );
