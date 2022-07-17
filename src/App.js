6af4335b0c5f2eec67f83615c39c65e6f6fcdf80
suraj.agariya@ext.religare.in <suraj.agariya@ext.religare.in>
2022-07-17 15:26:53 +0530

    "Done with Common css"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import './App.css';
 import Home from './components/Home/Home';
 import PageNotFound from './components/PageNotFound/PageNotFound'
 import MovieDetail from './components/MovieDetail/MovieDetail';
 import Header  from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import './App.scss'
 
 
 function App() {
   return (
     <Router>
       <Header></Header>
+      <main className='container'>
       <Routes>
       <Route path='/' exact element={<Home/>}/>
       <Route path='/movie/:mid' exact element={<MovieDetail/>}/>
-      <Route element={<PageNotFound/>}></Route>    
+      <Route element={<PageNotFound/>}></Route>  
+   
       </Routes>
+      </main>  
       <Footer></Footer>
     </Router>
   );
